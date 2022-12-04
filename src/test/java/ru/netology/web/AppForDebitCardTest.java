c9d9c0b412f34bcc9fac146c6abc3b2d3c54e5c0
Елена Кливер <volandmart@mail.ru>
2022-12-04 16:09:05 +0500

    final2

--- src/test/java/ru/netology/web/AppForDebitCardTest.java
+++ src/test/java/ru/netology/web/AppForDebitCardTest.java
@@ -1,4 +1,5 @@
 package ru.netology.web;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -18,13 +19,13 @@ public class AppForDebitCardTest {
 
 
     @BeforeAll
-    public static void BeforeAll(){
-        System.setProperty("webdriver.chrome.driver","driver/win/chromedriver.exe");
+    public static void BeforeAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
         //WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
-     public void BeforeEach() {
+    public void BeforeEach() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
@@ -32,23 +33,23 @@ public class AppForDebitCardTest {
         driver = new ChromeDriver(options);
 
     }
-     @AfterEach
-        public void afterEach(){
-            driver.quit();
-            driver = null;
-        }
 
-@Test
-   public void test (){
-    driver.get("http://localhost:9999");
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void test() {
+        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[type= \"text\"]")).sendKeys("Кравцов Сергей");
-    driver.findElement(By.cssSelector("[type= \"tel\"]")).sendKeys("+79562222959");
-    driver.findElement(By.className("checkbox__box")).click();
-    driver.findElement(By.className("button__text")).click();
-    String text = driver.findElement(By.className("paragraph")).getText();
-    assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",text.trim());
-}
-
+        driver.findElement(By.cssSelector("[type= \"tel\"]")).sendKeys("+79562222959");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
 
 
 }
